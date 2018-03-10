334443f0d1fa9b04cb8592cf5bc0006c50a6437b
Fabian <fabianwiles@live.com>
2018-03-10 13:04:32 +1300

    build: add secondary entrypoints for tokens

---
+++ modules/aspnetcore-engine/tokens/tokens.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { InjectionToken } from '@angular/core';
+
+export const REQUEST = new InjectionToken<string>('REQUEST');
+export const ORIGIN_URL = new InjectionToken<string>('ORIGIN_URL');
